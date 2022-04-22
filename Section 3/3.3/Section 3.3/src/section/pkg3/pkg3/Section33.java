e72812e99b0f0a5b8c8965cf72ac34175b772049
RikaJelita <97678576+RikaJelita@users.noreply.github.com>
2022-04-22 16:04:33 +0700

    Section 3.3

---
+++ Section 3/3.3/Section 3.3/src/section/pkg3/pkg3/Section33.java
@@ -0,0 +1,23 @@
+package section.pkg3.pkg3;
+
+
+public class Section33 {
+
+    public static void main(String[] args) {
+        
+   System.out.println("This is the first line."
+                    + "This is the still the first line."
+                    + "It's just that the line is very long "
+                    + "and I can't see it all in NetBeans."
+                    + "\n" + "This is the second line."
+                    + "\n" + "This is the third line.");
+   
+    System.out.println();
+    
+    System.out.println("This is the first line.");
+    System.out.println("This is the second line.");
+    System.out.println("This is the third line.");
+    System.out.println("This is the fourth line.");
+    }
+    
+}
